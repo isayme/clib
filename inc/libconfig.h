14d30429a3810727d10c715be467471b0f2ec84e
iSayme <isaymeorg@gmail.com>
2013-04-28 14:56:02 +0800

    Update libconfig.h

--- inc/libconfig.h
+++ inc/libconfig.h
@@ -4,7 +4,7 @@
 #include "defs.h"
 
 INT32 libconfig_open(char *cfg_path);
-INT32 libconfig_close()；
+INT32 libconfig_close();
 INT32 libconfig_get_cfg(char *key, char *value, INT32 value_len);
 
 INT32 get_cfg_from_file(char *key, char *value, INT32 value_len, char *cfg_path);
